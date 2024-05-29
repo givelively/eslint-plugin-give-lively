83b4e7097013aa2349a0a7017ebdcf1730cc5b4c
Christian Lowe <christglowe@gmail.com>
2024-05-29 15:56:00 -0500

    adds jest rules to givelively eslint configs

--- rules/jest.js
+++ rules/jest.js
@@ -1,9 +1,13 @@
 module.exports = {
-  "no-restricted-syntax": [
-    "error",
-    {
-      message: "Use userEvent instead of fireEvent",
-      selector: 'MemberExpression > Identifier[name="fireEvent"]',
-    },
-  ],
+  "jest/expect-expect": "error",
+  "jest/max-expects": ["error", { max: 3 }],
+  "jest/max-nested-describe": "error",
+  "jest/no-confusing-set-timeout": "error",
+  "jest/no-done-callback": "warn",
+  "jest/no-duplicate-hooks": "error",
+  "jest/prefer-hooks-in-order": "error",
+  "jest/prefer-hooks-on-top": "error",
+  "jest/prefer-to-be": "warn",
+  "jest/require-to-throw-message": "error",
+  "jest/require-top-level-describe": "error",
 };
